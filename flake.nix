047eb740f99115b163822765a8ced7c74b69f921
taichi765 <taichi0209.youtub@gmail.com>
2026-09-14 00:49:04 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Small CLI to manage local git repositories";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      packages = forAllSystems (
+        { pkgs }: {
+          default = pkgs.buildGoModule {
+            pname = "git-manage-utils";
+            version = "0.0.0";
+            src = ./.;
+          };
+        }
+      );
+    };
+}
